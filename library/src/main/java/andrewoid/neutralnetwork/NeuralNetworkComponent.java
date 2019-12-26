0b151beab6405f787dc031daf851ee21c7b353f5
HelenSutton <38437834+HelenSutton@users.noreply.github.com>
2019-12-26 18:53:11 -0500

    made slight changes to the display

--- library/src/main/java/andrewoid/neutralnetwork/NeuralNetworkComponent.java
+++ library/src/main/java/andrewoid/neutralnetwork/NeuralNetworkComponent.java
@@ -7,7 +7,7 @@ import java.awt.*;
 public class NeuralNetworkComponent extends JComponent {
     int start = 25;
     int size = 10;
-    int space = 100;
+    int space = 200;
     int space2 = 50;
     Network network;
     Color[] colors = {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, Color.CYAN, Color.MAGENTA, Color.PINK};
@@ -49,8 +49,8 @@ public class NeuralNetworkComponent extends JComponent {
     private void drawConnection(int row, int column, int number, Color color, Graphics g) {
         g.setColor(color);
         g.drawLine(start + ((row - 1) * space) + size,
-                start + (number * space2),
+                start + (number * space2) + (int)(size/2),
                 start + ((row) * space),
-                start + (column * space2));
+                start + (column * space2) + (int)(size/2));
     }
 }
